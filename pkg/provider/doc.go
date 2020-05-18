11da71def6f99f16a217860275d89e25ee54f96c
cmdallas <self@chrisdallas.tech>
2020-05-18 01:54:17 -0700

    chore(pkg): bootstrap rbitrageur and provider

---
+++ pkg/provider/doc.go
@@ -0,0 +1,7 @@
+/*
+
+	Package provider implements communication and utlities for cloud providers.
+
+*/
+
+package provider
---
+++ pkg/rbitrageur/doc.go
@@ -0,0 +1,8 @@
+/*
+
+	Package rbitrageur finds differences between prices in cloud spot markets
+	and implements functions to perform spatial arbitrage.
+
+*/
+
+package rbitrageur
